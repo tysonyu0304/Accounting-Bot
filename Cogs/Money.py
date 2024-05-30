362cdef6f39a3427dc746e6d927d61126708da40
tysonyu0304 <taiyou940304@gmail.com>
2024-05-30 12:17:50 +0800

    change dev device

--- Cogs/Money.py
+++ Cogs/Money.py
@@ -16,7 +16,7 @@ class Money(commands.Cog):
     #記帳指令
     async def buy(self, ctx, *args):
         #紀錄日期
-        self.date = list(map(int, str(datetime.datetime.now().strftime("%Y %m %d %H %M %S")).split()))
+        date = list(map(int, str(datetime.datetime.now().strftime("%Y %m %d %H %M %S")).split()))
     
          #判斷是否存在使用者資訊
         if not os.path.exists(f"Datas/{ctx.author.id}.json"):
@@ -33,7 +33,7 @@ class Money(commands.Cog):
             #判斷是否有輸入名稱
             #若沒有則使用"noname"作為名稱
             if len(args) == 1:
-                self.name = "noname"
+                name = "noname"
 
             #判斷格式是否錯誤 並記錄資料
             try:
@@ -43,8 +43,8 @@ class Money(commands.Cog):
                 await ctx.send("格式: $buy <價格> <名稱>(可省略)")
                 return
             
-            self.name = args[1]
-            data["log"][data["nextID"]] = [self.name, int(args[0]), self.date]
+            name = args[1]
+            data["log"][data["nextID"]] = [name, int(args[0]), date]
             data["nextID"] += 1
 
             #計算總金額
@@ -102,8 +102,6 @@ class Money(commands.Cog):
             #紀錄資料到json
             json.dump(data, open(f"Datas/{ctx.author.id}.json", "w"), indent = 4)
             await ctx.reply(f"已刪除 編號為 {args[0]} 的資料")
-
-            
     
     @commands.command(aliases=['t', 'T'])
     #查看總金額指令
@@ -132,6 +130,7 @@ class Money(commands.Cog):
                 sample = json.load(f)
                 sample["name"] = ctx.author.name
                 json.dump(sample, open(f"Datas/{ctx.author.id}.json", "w"), indent = 4)
+            return
         
         #讀取使用者資料
         with open(f"Datas/{ctx.author.id}.json", "r") as f:
@@ -140,20 +139,67 @@ class Money(commands.Cog):
             if len(data["log"]) == 0:
                 await ctx.reply("無資料")
             else:
-                #顯示紀錄
-                text = ""
-                j = 1
+                #一個頁面顯示的資料數
+                page_nums = data["page_nums"] 
+                #當前顯示頁面
+                current_page = 0 
+                #將json的dict轉成list 方便使用數字做為標籤
+                log_index = [] 
                 for i in data["log"]:
-                    self.date = data['log'][i][2]
-                    text += f"{j}. ID:{i}, 名稱:{data['log'][i][0]}, 價格:{data['log'][i][1]}, 日期:{self.date[0]}/{self.date[1]}/{self.date[2]}\n"
-                    j += 1
-                await ctx.reply(f"""
-```
-{text}
-總金額: {data['total']}
-```
-                """)
-    
+                    temp = data['log'][i]
+                    temp.insert(0, i)
+                    log_index.append(temp)
+
+                #算出總頁面數
+                total_page = -(-len(log_index) // page_nums)
+
+                #有多頁面時 處理不同頁數顯示的資料
+                def show_list(embed, current_page, page_nums):
+                    #算出當前頁面第一筆資料的標籤數字
+                    start_index = current_page * page_nums
+                    #為當前頁面上的資料排序
+                    j = 1
+                    #該頁總金額
+                    page_total = 0
+                    #處理該頁的embed訊息
+                    for i in range(start_index, start_index + page_nums):
+                        #縮短後續所需字數XD
+                        log = log_index[i]
+                        #依格式加入embed訊息
+                        embed.add_field(name=j, value=f"ID:{log[0]}, 名稱:{log[1]}, 價格:{log[2]}, 日期:{log[3][0]}/{log[3][1]}/{log[3][2]}", inline=False)
+                        #計算金額
+                        page_total += log[2]
+                        j += 1
+
+                    #回傳embed訊息和頁面總金額
+                    return embed, page_total
+                    
+                #總頁數大於1
+                if total_page > 1:
+                    embed = discord.Embed(title=f"{self.bot.get_user(ctx.author.id)} 的帳本")
+                    print("before def")
+                    embed, page_total = show_list(embed, current_page, page_nums)
+                    print("After def")
+                    embed.add_field(name="", value="", inline=False)
+                    embed.add_field(name="總金額", value=page_total, inline=False)
+                    print("After add")
+                    await ctx.reply(embed=embed)
+                    print("done") # for test
+
+                else:
+                    #顯示紀錄
+                    embed = discord.Embed(title=f"{self.bot.get_user(ctx.author.id)} 的帳本")
+                    j = 1
+                    for i in range(len(log_index)):
+                        log = log_index[i]
+                        # embed.add_field(name="", value=f"{j}. 名稱:{log[1]}, 價格:{log[2]}, 日期:{log[3][0]}/{log[3][1]}/{log[3][2]}", inline=False)
+                        embed.add_field(name=j, value=f"名稱:{log[1]}\n價格:{log[2]}\n日期:{log[3][0]}/{log[3][1]}/{log[3][2]}", inline=True)
+                        # text += f"{j}. ID:{log[0]}, 名稱:{log[1]}, 價格:{log[2]}, 日期:{log[3][0]}/{log[3][1]}/{log[3][2]}\n"
+                        j += 1
+                    embed.add_field(name="", value="", inline=False)
+                    embed.add_field(name="總金額", value=data["total"], inline=False)
+                    await ctx.reply(embed=embed)
+                
     @commands.command(aliases=['e', 'E'])
     #編輯數據指令
     async def edit(self, ctx, *args):
